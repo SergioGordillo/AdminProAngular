cae9e964706041ad7676ce2e7e4100046c5fc5b7
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-11-27 17:55:23 +0100

    feat: structure of header, sidebar and footer implemented

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -4,6 +4,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: [
+    `
+    .has-arrow.waves-effect.waves-dark.active {
+      background: transparent;
+    }
+    `
   ]
 })
 export class SidebarComponent implements OnInit {
